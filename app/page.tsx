ebbc91cbdfeab7ab0520cf5e1f63b2797cc6c4f9
Mikiyaskelifa25 <119225706+Mikiyaskelifa25@users.noreply.github.com>
2024-10-10 21:52:57 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -96,23 +96,7 @@ const options = {
     datalabels: {
       display: true,
       align: "left",
-      formatter: (value: number, context) => {
-        const dataset = context.chart.data.datasets[0].data;
-        const maxValue = Math.max(...dataset);
-        const { width, height, ctx } = context.chart;
-        ctx.restore();
-        const fontSize = (height / 114).toFixed(2);
-        ctx.font = `${fontSize}em sans-serif`;
-        ctx.textBaseline = "middle";
-        ctx.fillStyle = "black";
-        const text = "550";
-        const textX = Math.round((width - ctx.measureText(text).width) / 2);
-        const textY = height / 2;
-        ctx.fillText(text, textX, textY);
-        ctx.save();
-        console.log(ctx);
-        return value === maxValue ? ` ${value}` : "";
-      },
+    
       font: {
         size: "16",
         weight: "bold",
